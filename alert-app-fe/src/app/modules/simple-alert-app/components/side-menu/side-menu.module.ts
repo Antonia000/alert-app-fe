49948de3489b33daf212125c6b6eee460644f95a
Antonia000 <nicolaenicoleta31@gmail.com>
2024-08-11 14:17:44 +0300

    added caching interceptor

--- alert-app-fe/src/app/modules/simple-alert-app/components/side-menu/side-menu.module.ts
+++ alert-app-fe/src/app/modules/simple-alert-app/components/side-menu/side-menu.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SideMenuComponent } from './side-menu.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SideMenuComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule],
   exports: [SideMenuComponent],
 })
 export class SideMenuModule {}
