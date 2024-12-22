67b51565bc8368b2530b1a3fa5e4c388b88eb043
Antonia000 <nicolaenicoleta31@gmail.com>
2024-12-22 16:18:48 +0200

    added env files

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  be: 'https://europe-west2-alert-app-d1905.cloudfunctions.net',
+  firebase: {},
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  be: 'https://europe-west2-prod-alert-app.cloudfunctions.net',
+  firebase: {},
+};
